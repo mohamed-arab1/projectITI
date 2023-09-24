e472c60cebb78ee9131699051537096b419986aa
Omarashraf2002 <omarash2020@gmail.com>
2023-09-24 13:12:58 +0300

    Movie&Home

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return <div id="intro" className=""></div>;
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TvShow from "./Components/TvShow/TvShow";
+import TvDetails from "./Components/TvDetails/TvDetails";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tv" element={<TvShow />} />
+      <Route path="/tvdetails/:id" element={<TvDetails />} />
+    </Routes>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
